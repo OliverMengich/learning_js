5cabf57ca8b37197dae1d4928d42a36fd793cc0d
OliverMengich <kemeioliver2000@gmail.com>
2022-11-15 09:15:07 +0300

    Incorporates exercises questions on ARRAY.REDUCE() METHOD

--- index.js
+++ index.js
@@ -328,3 +328,127 @@ function map(list, fn) {
 //     }
 //     return newArr;
 // }, []))
+//=============================================ARRAY.REDUCE PRACTICE QUESTIONS========================
+//1. Turn an array of numbers into a totla of all the numbers
+function total(arr) {
+    //code here 
+    return arr.reduce((accumulator, item) => {
+        return accumulator + item;
+    })
+}
+console.log(total([1, 2, 3, 9, 4, 9]));
+//2. Turn an array of number into a long string of all the numbers
+function stringConcat(arr) {
+    return arr.reduce(function (accumulator, item) {
+        return (accumulator+''+item).toString()
+    })
+}
+console.log(stringConcat([1, 2, 3, 7, 9, 3, 4])); 
+//3. Turn an Array of voter objets into a coun of how many people voted
+function totalVotes(arr) {
+    //code here
+    return arr.reduce(function (acc, item) {
+        if (item.voted === true) {
+            // console.log(item.name+' :voted');
+            acc += 1;
+        }
+        return acc;
+    },0)
+}
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+console.log(totalVotes(voters));
+//4. Given an array of all your wishlist items,
+//figure out how much it would cost to just buy everything at once
+function shoppingSpree(arr) {
+    //code here
+    return arr.reduce(function (acc,item) {
+        return acc + item.price;
+    },0)
+}
+var wishlist = [
+    { title: "Tesla Model S", price: 90000 },
+    { title: "4 carat diamond ring", price: 45000 },
+    { title: "Fancy hacky Sack", price: 5 },
+    { title: "Gold fidgit spinner", price: 2000 },
+    { title: "A second Tesla Model S", price: 90000 }
+];
+
+console.log(shoppingSpree(wishlist));
+//5. Given an array of arrays, flatten them into a single array
+function flatten(arr) {
+   // your code here    
+    return arr.reduce(function (acc,item) {
+        if (Array.isArray(item)) {
+            acc.push(...item);
+        } else {
+            acc.push(item)
+        }
+        return acc;
+    },[])
+}
+var arrays = [ ["1", "2", "3"], [true],[4, 5, 6], 89];
+
+console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+
+//6. Given an array of potential voters, return an object representing the results of the vote
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+function voterResults(arr) {
+   // your code here
+    return arr.reduce(function (acc,item) {
+        if (item.age>=18 && item.age<=25) {
+            if (item.voted) {
+                acc['numYoungVotes'] += 1;
+            }
+            acc.numYoungPeople += 1;
+        } else if(item.age>=26 && item.age<=35) {
+            if (item.voted) {
+                acc.numMidVotesPeople += 1;
+            }
+            acc.numMidsPeople += 1;
+        } else if(item.age>=36 && item.age<=55) {
+            if (item.voted) {
+                acc.numOldVotesPeople += 1;
+            }
+            acc.numOldPeople += 1;
+        }
+        return acc;
+    }, { numYoungVotes: 0, numYoungPeople: 0,numMidVotesPeople:0, numMidsPeople:0, numOldVotesPeople:0, numOldPeople:0 })
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+/*
+{ numYoungVotes: 1,
+  numYoungPeople: 4,
+  numYoungPeople: 3,
+  numMidsPeople: 4,
+  numOldVotesPeople: 3,
+  numOldVotesPeople: 4 
+}
+*/
